eb3fdf0f49691d2115f61152a43337ba424e8e46
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2022-09-28 15:53:57 +0900

    weapon

--- CapStone/Assets/02. Scripts/Weapon/RaycastWeapon.cs
+++ CapStone/Assets/02. Scripts/Weapon/RaycastWeapon.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class RaycastWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isFiring = false;
+
+    public void StartFiring()
     {
-        
+        isFiring = true;
     }
 
     // Update is called once per frame
-    void Update()
+    public void StopFiring()
     {
-        
+        isFiring = false;
     }
 }
